fb4cc4b81e1e9628678bd5a1a7de0d206e2fcc21
Alan-Blanchet <alanblanchet@orange.fr>
2020-09-11 16:46:53 +0200

    reorganised the directories

--- sprint4/1/main.py
+++ sprint4/1/main.py
@@ -1,14 +1,3 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
-
-finalTab = np.zeros((180,180,4))
-
-cheval = plt.imread("dark-knight.png","PNG")
-finalTab = np.copy(cheval)
-tab2 = np.copy(cheval)
-img = np.block([finalTab, tab2])
-print(img)
-print(img.shape)
-plt.imshow(img)
-plt.show()
