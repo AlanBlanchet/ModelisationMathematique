c67bc23e826f7c8cb62c934518e51a39ee952a4c
Alan-Blanchet <alanblanchet@orange.fr>
2020-09-12 16:52:26 +0200

    Done photomaton challenge

--- sprint1/1 photomaton/main.py
+++ sprint1/1 photomaton/main.py
@@ -2,13 +2,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-finalTab = np.zeros((180,180,4))
+x = int(input("Choisissez le photomaton :\n1 = doubler horizontal\n2 = double vertical\n3 = quadruple\n:"))
 
-cheval = plt.imread("dark-knight.png","PNG")
-finalTab = np.copy(cheval)
-tab2 = np.copy(cheval)
-img = np.block([finalTab, tab2])
-print(img)
-print(img.shape)
-plt.imshow(img)
+cheval = plt.imread("../../images/dark-knight.png","PNG")
+
+def horizontal():
+    global cheval
+    cheval = np.block([[cheval], [cheval]])
+
+def vertical():
+    global cheval
+    cheval = np.block([[[cheval]], [[cheval]]])
+
+if x == 1:
+    horizontal()
+elif x == 2:
+    vertical()
+elif x == 3:
+    horizontal()
+    vertical()
+
+plt.imshow(cheval)
 plt.show()
