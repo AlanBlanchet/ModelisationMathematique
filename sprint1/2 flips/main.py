a7f67444f50a7b36d788fdfcc46907fb23dd0086
Alan-Blanchet <alanblanchet@orange.fr>
2020-09-12 17:15:14 +0200

    Done challenge flips

--- sprint1/2 flips/main.py
+++ sprint1/2 flips/main.py
@@ -1,3 +1,33 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+
+x = int(input("Choisissez le flip :\n1 = flip horizontal\n2 = flip vertical\n3 = quadruple avec flip vertical\n:"))
+
+cheval = plt.imread("../../images/dark-knight.png","PNG")
+
+def doubleHorizontal(img):
+    return np.block([[img], [img]])
+
+def doubleVertical(img):
+    return np.block([[[img]], [[img]]])
+
+def flipHorizontal(img):
+    return img[:,::-1]
+
+def flipVertical(img):
+    return img[::-1]
+
+if x == 1:
+    cheval = flipHorizontal(cheval)
+elif x == 2:
+    cheval = flipVertical(cheval)
+elif x == 3:
+    flipV = flipVertical(cheval)
+    cheval = np.block([[[cheval]],[[flipV]]])
+    cheval = doubleHorizontal(cheval)
+else:
+    print("Ce n'est pas une option.")
+
+plt.imshow(cheval)
+plt.show()
