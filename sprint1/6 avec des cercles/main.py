36142a4fd90c1fc46c5f4598879c1114578d4f12
Mateo Alexis <alexis.mateo@etu.umontpellier.fr>
2020-09-18 15:02:00 +0200

    debut cercle

--- sprint1/6 avec des cercles/main.py
+++ sprint1/6 avec des cercles/main.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+
+render = np.zeros((50,50,3), dtype=np.uint8)
+
+def cercle(render, r):
+    for x in range(50):
+        bord = np.sqrt(r * r - x * x)
+        render[bord:bord,x:x,:] = [255,0,0]
+
+plt.imshow(render)
+plt.show()
