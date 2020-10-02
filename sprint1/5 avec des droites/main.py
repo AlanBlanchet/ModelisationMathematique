fb1654421f02d143ecad9938aafb62e3d249bdd9
Mateo Alexis <alexis.mateo@etu.umontpellier.fr>
2020-10-02 16:16:35 +0200

    droite avec équation

--- sprint1/5 avec des droites/main.py
+++ sprint1/5 avec des droites/main.py
@@ -1,12 +1,28 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-import matplotlib.pyplot as plt
 import numpy as np
-qualite = 256
+import matplotlib.pyplot as plt
 
-render = np.zeros((qualite,qualite,3), dtype=np.uint8)
-for x in range(qualite):
-    render[x-1:x,x-1:x,:] = [255,90,45]
-    
-plt.imshow(render)
+width = 300
+height = 200
+
+x = np.arange(width)
+y = np.arange(height - 1, -1, -1)
+
+X, Y = np.meshgrid(x, y)
+
+y1 = np.linspace(2/3, height, width)
+y2 = np.linspace(height, 2/3, width)
+
+enveloppe = np.zeros((height, width, 3), dtype = np.uint8)
+
+enveloppe_bleue = (y2 < Y) & (y1 < Y)
+enveloppe_gris = (y2 > Y) & (y1 < Y)
+
+
+enveloppe[(y2 < Y) & (y1 < Y),:] = [31, 119, 180]
+enveloppe[(y2 > Y) & (y1 < Y),:] = [127, 127, 127]
+enveloppe[(y2 > Y) & (y1 > Y),:] = [44, 160, 44]
+enveloppe[(y2 < Y) & (y1 > Y),:] = [255, 127, 14]
+enveloppe[199,299] = [255, 127, 14]
+
+plt.imshow(enveloppe)
 plt.show()
